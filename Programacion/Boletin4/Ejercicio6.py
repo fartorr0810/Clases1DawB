7c236eaced55fb4392274ea8ec47d1eb4771d9c5
Fran <xXxArroyoxXx>
2020-11-26 21:42:35 +0100

    Cosas nuevas

--- Programacion/Boletin4/Ejercicio6.py
+++ Programacion/Boletin4/Ejercicio6.py
@@ -1,16 +1,34 @@
 '''
-Created on 25 Nov 2020
+Created on 26 nov. 2020
 
-@author: estudiante
-6.Realizar una función que busque una palabra escondida dentro de un texto. 
-Por ejemplo, si la cadena es “shybaoxlna” y la palabra que queremos buscar es “hola”, 
-entonces si se encontrará y deberá devolver True, en caso contrario deberá devolver False.
+@author: fran
 '''
-busqueda="z"
-cadena="shybaoxknah"
-def buscador(cadena,busqueda):
-    for i in busqueda:
-        for i in cadena:
-            if cadena[i]==busqueda[i]:
-                return True
-            
+cadena="shy"
+busqueda="hola"
+def buscadorr(cadena):
+    contadorbusqueda=0
+    contadorcadena=0
+    while contadorbusqueda<len(busqueda) and contadorcadena<len(cadena):
+        letracadena=len(cadena)
+        letrabusqueda=len(busqueda)
+        if letrabusqueda==letracadena:
+            contadorbusqueda+=1
+            contadorcadena+=1
+        else:
+            contadorcadena+=1
+    if contadorcadena==len(cadena):
+        return True
+print(buscadorr(cadena))
+# def buscador(cadena):
+#     resultado=""
+#     i=1
+#     j=1
+#     while busqueda!=resultado:
+#         if cadena[i]==busqueda[j]:
+#             i+=1
+#             j+=1
+#             resultado=+j
+#         else:
+#             i+=1
+#     return True
+# print(buscador(cadena))
---
+++ Programacion/Boletin4/Ejercicio7.py
@@ -0,0 +1,11 @@
+'''
+Created on 26 nov. 2020
+
+@author: fran
+
+'''
+from pydoc import replace
+cadena="Estimado nombre apellido"
+remplazador="Francisco Arroyo"
+cadenanueva=replace(cadena,remplazador)
+print(cadenanueva)
