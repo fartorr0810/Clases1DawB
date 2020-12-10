ea08e2de94cb1db34a451005902d2af160b651ff
Fran <xXxArroyoxXx>
2020-12-10 20:24:47 +0100

    Cosas de casa

--- Programacion/bletin31/ejercicio8.py
+++ Programacion/bletin31/ejercicio8.py
@@ -10,11 +10,11 @@ import math
 def secondOrder(a=1,b=10,c=9):
     x=(b**2)-(4*a*c)
     if x==0:
-        x1=(-b)/(2*a)
-        return "Una solucion"+str(x1)
+        solucion1=(-b)/(2*a)
+        return "Una solucion"+str(solucion1)
     else:
-        x1=(-b+math.sqrt(x)/(2*a))
-        x2=(-b-math.sqrt(x)/(2*a))
-        return "Dos soluciones"+str(x1)+" y "+str(x2)
+        solucion1=(-b+math.sqrt(solucion1)/(2*a))
+        solucion2=(-b-math.sqrt(x)/(2*a))
+        return "Dos soluciones"+str(solucion1)+" y "+str(solucion2)
     
 print(secondOrder())
