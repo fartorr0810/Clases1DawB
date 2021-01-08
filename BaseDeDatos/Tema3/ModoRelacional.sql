752c00bdc269082fc30d6c9577e00840830982ce
Fran <xXxArroyoxXx>
2021-01-08 14:53:45 +0100

    End work

--- BaseDeDatos/Tema3/ModoRelacional.sql
+++ BaseDeDatos/Tema3/ModoRelacional.sql
@@ -1,49 +1,69 @@
 CREATE TABLE EmpresaSeguridad(
-cif text(15)
-PRIMARY KEY (cif),
-);
+	cif text(15)
+	PRIMARY KEY (cif),
+	);
 CREATE TABLE Guardias(
-idguardia   number(5),
-nombre  text,
-apellidos   text,
-cif text(15) ADD REFERENCES EmpresaSeguridad(cif),
-PRIMARY KEY (idguardia)
-);
+	idguardia   number(5),
+	nombre  text,
+	apellidos   text,
+	cif text(15) ADD REFERENCES EmpresaSeguridad(cif),
+	PRIMARY KEY (idguardia)
+	);
 CREATE TABLE SUCURSAL(
-codigo  number(4), 
-direccion   text,
-telefono    text,
-ciudad  text,
-provincia   text
-PRIMARY KEY (codigo)
-);
+	codigo  number(4), 
+	direccion   text,
+	telefono    text,
+	ciudad  text,
+	provincia   text
+	PRIMARY KEY (codigo)
+	);
 
 CREATE TABLE EMPLEADO(
-idtrabajador    number(5),
-nif text,
-nombre  text,
-apellido    text,
-telefono    text,
-direccion   text,
-codigo_sucursal number(4) ADD REFERENCES SUCURSAL(codigo),
-PRIMARY KEY (idtrabajador)
-);
+	idtrabajador    number(5),
+	nif text,
+	nombre  text,
+	apellido    text,
+	telefono    text,
+	direccion   text,
+	codigo_sucursal number(4) ADD REFERENCES SUCURSAL(codigo),
+	PRIMARY KEY (idtrabajador)
+	);
 CREATE TABLE REVISTA(
     idrevista   number(5),
     título  text,
     tipo    text,
     periodicidad    date,
     PRIMARY KEY (idrevista)
-);
+	);
 CREATE TABLE SucursalPublicaRevista(
-    codigo_sucursal, 
-    numero_registro_revista
+    codigo_sucursal	number(4) add REFERENCES SUCURSAL(codigo), 
+    idrevista	number(5) add REFERENCES REVISTA(idrevista)
     );
 
-    PERIODISTA(id, nombre, apellido1, apellido2, teléfono, especialidad)
+CREATE TABLE PERIODISTA(
+	idperiodista 	number(4),
+    nombre	text,
+    apellido text,
+    teléfono	text,
+    especialidad	text
+    PRIMARY KEY (idperiodista)
+    );
 
-    PERIODISTA_ESCRIBE_REVISTA(id_periodista, número_registro_revista)
-    SECCIÓN(id, título, extensión, número_registro_revista)
-        número_registro_revista: FOREIGN KEY de REVISTA(número_registro)
-    EJEMPLAR(id, número_ejemplares, número_páginas, fecha, número_registro_revista)
-        número_registro_revista: FOREIGN KEY de REVISTA(número_registro)
+CREATE TABLE Periodista_Escribe_Revista(
+	idperiodista	number(4) add REFERENCES PERIODISTA(idperiodista),
+	idrevista	number(5) add REFERENCES REVISTA(idrevista)
+	);
+
+CREATE TABLE SECCION(
+	idseccion	number(4) PRIMARY KEY,
+	titulo	text,
+	descripcion	text,
+	idrevista number(5) add REFERENCES REVISTA(idrevista)
+	);
+CREATE TABLE EJEMPLAR(
+	idejemplar	number(5) PRIMARY KEY,
+	num_ejemplares	number(10), 
+	numpaginas	(3), 
+	fecha	date, 
+	idrevista	number(5) add REFERENCES REVISTA(idrevista)
+	);
---
+++ BaseDeDatos/Tema3/ScriptDeCreacion.sql
@@ -0,0 +1,85 @@
+CREATE TABLE EmpresaSeguridad(
+	cifseguridad varchar(15),
+	PRIMARY KEY (cifseguridad)
+	);
+CREATE TABLE Guardias(
+	idguardia   number(5),
+	nombre  varchar2(30),
+	apellidos   varchar2(30),
+	cifseguridad varchar2(15),
+	PRIMARY KEY (idguardia),
+	FOREIGN KEY (cifseguridad) REFERENCES EmpresaSeguridad
+	);
+CREATE TABLE SUCURSAL (
+	codsucursal number(4), 
+	direccion   varchar2(30),
+	telefono    varchar2(30),
+	ciudad  varchar2(30),
+	provincia   varchar2(30),
+	cifseguridad varchar2(15),
+	FOREIGN KEY (cifseguridad) REFERENCES EmpresaSeguridad,
+	PRIMARY KEY (codsucursal)
+	);
+
+CREATE TABLE EMPLEADO(
+	idtrabajador    number(5),
+	nif varchar2(9),
+	nombre  varchar2(30),
+	apellido    varchar2(30),
+	telefono    varchar2(30),
+	direccion   varchar2(30),
+	codigo_sucursal number(4),
+	FOREIGN KEY (codigo_sucursal) REFERENCES SUCURSAL,
+	PRIMARY KEY (idtrabajador)
+	);
+CREATE TABLE REVISTA(
+    idrevista   number(5),
+    título  varchar2(30),
+    tipo    varchar2(30),
+    periodicidad    date,
+    PRIMARY KEY (idrevista)
+	);
+CREATE TABLE SucursalPublicaRevista(
+    codsucursal	number(4),
+    idrevista	number(5),
+    FOREIGN KEY (idrevista) REFERENCES REVISTA,
+	FOREIGN KEY (codsucursal) REFERENCES SUCURSAL, 
+    PRIMARY KEY(codsucursal,idrevista)
+
+    );
+
+CREATE TABLE PERIODISTA(
+	idperiodista 	number(4),
+    nombre	varchar2(30),
+    apellido varchar2(30),
+    telefono	varchar2(10),
+    especialidad	varchar2(30),
+ 	tiponacionalidadperiodista	varchar2(10),
+    PRIMARY KEY (idperiodista)
+    );
+
+CREATE TABLE Periodista_Escribe_Revista(
+	idperiodista	number(4),
+	idrevista	number(5),
+	FOREIGN KEY (idperiodista) REFERENCES PERIODISTA,
+	FOREIGN KEY (idrevista) REFERENCES REVISTA,
+	PRIMARY key (idperiodista,idrevista)
+	);
+
+CREATE TABLE SECCION(
+	idseccion	number(4),
+	titulo	varchar2(30),
+	descripcion	varchar2(30),
+	idrevista number(5),
+	FOREIGN KEY (idrevista) REFERENCES REVISTA,
+	PRIMARY KEY (idseccion)
+	);
+CREATE TABLE EJEMPLAR(
+	idejemplar	number(5),
+	num_ejemplares	number(10), 
+	numpaginas	number(3), 
+	fecha	date, 
+	idrevista	number(5),
+	FOREIGN KEY (idrevista) REFERENCES REVISTA,
+	PRIMARY KEY (idrevista)
+	);
