04537a1767af6b6585452956cb93c089d1c3b531
franarroyo <frankarroyop@gmail.com>
2021-04-05 10:54:12 +0200

    Ejercicio 1BIS de plsql  hecho

--- BaseDeDatos/PLSQL/Boletin1BIS.sql
+++ BaseDeDatos/PLSQL/Boletin1BIS.sql
@@ -5,9 +5,11 @@ DECLARE...BEGIN...END
 BEGIN  
 	DBMS_OUTPUT.PUT_LINE('Hola');
 	END; 
+
 /* 2.-Escribir  un  bloque  PL/SQL  que  cuente  el  número  de  filas 
  que  hay  en  la  tabla productos, deposita el resultado en 
  la variable v_num, y visualiza su contenido. */
+
 DECLARE 
 contadordeproductos	NUMBER;
 BEGIN
@@ -17,6 +19,7 @@ BEGIN
 
 /* CREATE OR REPLACE PROCEDURE/ FUNTION
 1)Escribir un procedimiento que reciba dos números y visualice su suma. */
+
 CREATE OR REPLACE 
 PROCEDURE SUMA (num1 number,num2 number) IS 
 BEGIN
@@ -114,6 +117,77 @@ call SUMAR5NUMEROS(2,2,2,2,2);
 /* 9) Escribir una función que devuelva solamente caracteres alfabéticos sustituyendo
  cualquier otro carácter por blancos a partir de una cadena que se pasará en la llamada.
  */
+CREATE OR REPALCE FUNCTION substituircaracterporblanco (cad VARCHAR2)
+RETURN VARCHAR2 as 
+	aux char;
+	cadenanueva	VARCHAR2(30);
+BEGIN 
+for i in 1..LENGTH(cad) LOOP
+	aux:=SUBSTR(cad,i,1);
+	if (ASCII(aux)) NOT BETWEEN 65 AND 90) AND (ASCII(aux) not between 97 and 112)
+THEN
+	aux:=' ';
+	END IF;
+	cadenanueva:= nuevacadena||aux;
+	end loop;
+	return cadenanueva;
+END substituircaracterporblanco;
+
+/* 11. Implementar un procedimiento que reciba un importe y visualice el desglose del 
+cambio en unidades monetarias de 1, 5, 10, 25, 50, 100, 200, 500, 1000, 2000, 5000 Ptas.
+ en orden inverso al que aparecen aquí enumeradas.  */
+CREATE OR REPLACE PROCEDURE desglosador(dinero number)
+AS 
+	cambio FLOAT := dinero;
+	moneda float;
+	v_uni_moneda float;
+BEGIN
+	DBMS_OUTPUT.PUT_LINE('DESGLOSADOR DE '+dinero);
+	while cambio>0 LOOP
+		if cambio>= 5000 then 
+		moneda:=5000;
+		ELSIF cambio >= 2000 THEN
+			moneda := 2000;
+         	ELSIF cambio >= 1000 THEN
+			moneda := 1000;
+	   	ELSIF cambio >= 500 THEN
+			moneda := 500;
+         	ELSIF cambio >= 200 THEN
+			moneda := 200;
+	   	ELSIF cambio >= 100 THEN
+			moneda := 100;
+         	ELSIF cambio >= 50 THEN
+			moneda := 50;
+	   	ELSIF cambio >= 25 THEN
+			moneda := 25;
+	   	ELSIF cambio >= 10 THEN
+			moneda := 10;
+         	ELSIF cambio >= 5 THEN
+			moneda := 5;
+	   	ELSE  
+			moneda := 1;
+END IF;
+	v_uni_moneda:= TRUNC(cambio/moneda);
+DBMS_OUTPUT.PUT_LINE(v_uni_moneda || 'Hay ' || moneda || ' euros. ');
+ 		cambio := MOD(cambio, moneda);
+	END LOOP;
+END desglosador;
+
+/* 12) Escribir un procedimiento que modifique la localidad de un departamento. El procedimiento recibirá 
+como parámetros el número del departamento y la localidad nueva
+ */
+CREATE OR REPLACE PROCEDURE CAMBIARLOCALIDAD(
+	num_depart NUMBER,
+	localidad VARCHAR2)
+AS
+BEGIN
+	UPDATE departamento SET localidad = localidadd
+		WHERE numerodepartamento = num_depart;
+END CAMBIARLOCALIAD;
 
 
-
+/* 13) Visualizar todos los procedimientos y funciones del usuario almacenados en la base de datos y su situación (valid o invalid).
+ */
+ SELECT *
+FROM user_objects 
+WHERE OBJECT_TYPE IN ('PROCEDURE','FUNCTION');
